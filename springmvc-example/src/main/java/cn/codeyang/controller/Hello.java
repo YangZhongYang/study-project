5f82bb82119e095f4d22b165815eef9561acc0ea
yangzhongyang <yangzhongyang@meicai.cn>
2016-01-22 13:09:58 +0800

    springmvc简单使用

--- springmvc-example/src/main/java/cn/codeyang/controller/Hello.java
+++ springmvc-example/src/main/java/cn/codeyang/controller/Hello.java
@@ -2,13 +2,16 @@ package cn.codeyang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by meicai on 2016/1/21
  */
 @Controller
+@RequestMapping("/blog")
 public class Hello {
 
     @RequestMapping("/hello1.do")
@@ -21,6 +24,15 @@ public class Hello {
     @RequestMapping("/hello2.do")
     public String query2(Model model){
         model.addAttribute("name", "zhangsan");
+
+        return "hello";
+    }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getUser(Model model, @PathVariable Long id){
+        System.out.println(id);
+        model.addAttribute("name", "delete");
 
         return "hello";
     }
